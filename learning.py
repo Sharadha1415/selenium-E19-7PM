a7a338375a56a31ef0564e5c8213eedc1ed13825
Sharada1415 <ramyar.alpha@gmail.com>
2025-11-18 20:20:20 +0530

    defined class

--- learning.py
+++ learning.py
@@ -1,8 +1,11 @@
-if 10%2==0:
-    print("even")
-else:
-    print("odd")
+class Numbers:
 
+    def even_odd(self, start, end):
+        for ele in range(start, end):
+            if ele%2==0:
+                print("even")
+            else:
+                print("odd")
 
-
-
+num = Numbers()
+num.even_odd(10, 20)
